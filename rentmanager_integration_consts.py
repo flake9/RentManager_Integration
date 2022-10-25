c50641c0cde6cac0dcc8e6a533881c97f2a715b6
bhargav <tavethiyabhargav@gmail.com>
2022-10-25 13:41:20 +0530

    Removed credentials

--- rentmanager_integration_consts.py
+++ rentmanager_integration_consts.py
@@ -1,6 +1,6 @@
 # Credential constants
-RENTMANAGER_API_USERNAME = 'ANguyen'
-RENTMANAGER_API_PASSWORD = 'Abcd@1234'
+RENTMANAGER_API_USERNAME = ''
+RENTMANAGER_API_PASSWORD = ''
 
 # Other constants
 RENTMANAGER_BASE_URL = 'https://bobai.api.rentmanager.com'
